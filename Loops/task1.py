48ff03e03a27cbfb132d2cd8eb0197d1445391fd
Mennahassann <mennahassaneissa@gmail.com>
2026-05-04 23:19:12 +0300

    Task 1 edit

--- Loops/task1.py
+++ Loops/task1.py
@@ -3,19 +3,39 @@ from common.parser import parse
 
 class Task1Loops:
     def run(self, path):
-        sizes = []
+        file = open(path, "r", encoding="utf-8")
 
-        with open(path, "r", encoding="utf-8") as file:
-            for line in file:
-                parsed = parse(line)
-                if parsed:
-                    sizes.append(parsed["size"])
+        min_size = None
+        max_size = None
+        total = 0
+        count = 0
+        for line in file:   #processing one line at a time
+            parsed = parse(line)
 
-        min_size = min(sizes)
-        max_size = max(sizes)
-        avg_size = sum(sizes) / len(sizes)
+            if not parsed:
+                continue
+            size = parsed["size"]
+            if min_size is None:
+                min_size = size
+                max_size = size
 
-        result = f"Min size: {min_size}\nMax size: {max_size}\nAvg size: {avg_size}"
+            if size < min_size:
+                min_size = size
 
+            if size > max_size:
+                max_size = size
+
+            total += size
+            count += 1
+
+        file.close()
+
+        avg_size = total / count if count > 0 else 0
+
+        result = (
+            f"Min size: {min_size}\n"
+            f"Max size: {max_size}\n"
+            f"Avg size: {avg_size}"
+        )
         print(result)
         return result
--- RDD/task1.py
+++ RDD/task1.py
@@ -1,6 +1,5 @@
 from common.parser import parse
 
-
 class Task1:
     def run(self, sc, path):
         data = sc.textFile(path)
@@ -8,11 +7,22 @@ class Task1:
         parsed = data.map(parse).filter(lambda x: x is not None)
         sizes = parsed.map(lambda x: int(x["size"]))
 
-        min_size = sizes.min()
-        max_size = sizes.max()
-        avg_size = sizes.mean()
+        result = sizes.map(lambda x: (x, x, x, 1)) \
+                        .reduce(lambda a, b: (
+                            min(a[0], b[0]),   # min
+                            max(a[1], b[1]),   # max
+                            a[2] + b[2],       # sum
+                            a[3] + b[3]        # count
+                        ))
 
-        result = f"Min size: {min_size}\nMax size: {max_size}\nAvg size: {avg_size}"
+        min_size, max_size, total, count = result
+        avg_size = total / count if count != 0 else 0
 
-        print(result)
-        return result
+        output = (
+            f"Min size: {min_size}\n"
+            f"Max size: {max_size}\n"
+            f"Avg size: {avg_size}"
+        )
+
+        print(output)
+        return output
